e36ad39577244df489b59a74ff0a9d6ba40aadd9
Hello Tunmbi <hellotunmbi@gmail.com>
2017-11-04 21:34:54 +0100

    edit server.js

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ app.get('/api', (request, response) => {
 
 /* CONFIGURE HOSTNAME AND PORT  */
 const hostname = "https://studentia-api.herokuapp.com/";
-const port = 3001;
+const port = 5000;
 
-app.listen(process.env.PORT || 5000, hostname, () => {
+app.listen(port, hostname, () => {
 
     mongoose.connect(mongooseUri, dbOptions, (err) => {
         if (err) {
