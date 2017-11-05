825ee790a4cb9a89f4b3b359d97b023c027cce86
Hello Tunmbi <hellotunmbi@gmail.com>
2017-11-05 04:44:38 +0100

    removed routes

--- server.js
+++ server.js
@@ -10,38 +10,122 @@ const uriUtil = require('mongodb-uri');
 
 const mongodbUri = 'mongodb://student:password@ds143245.mlab.com:43245/studentia-mongo';
 const mongooseUri = uriUtil.formatMongoose(mongodbUri);
-
 const dbOptions = { useMongoClient: true };
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const students = require('./data');
+const Student = require('./api/students/model/Student');
 
-
-/* GET LIST OF ALL STUDENTS INFO  */
-app.use('/api/students', require('./api/students/routes/post_student'));
-app.use('/api/students', require('./api/students/routes/get_students'));
-app.use('/api/students', require('./api/students/routes/get_student'));
-app.use('/api/students', require('./api/students/routes/delete_student'));
-app.use('/api/students', require('./api/students/routes/put_student'));
+/** *********************************************************** */
 
 app.get('/', (request, response) => {
-    response.json({ message: 'You successfully connected.'})
+    response.status(200).json({ message: 'You successfully connected.'})
 });
+
+/** *********************************************************** */
 
 app.get('/api', (request, response) => {
-    response.json({ message: 'Welcome to API Home.'})
+    response.status(200).json({ message: 'Welcome to API Home.'})
 });
 
+/** *********************************************************** */
 
+/** GET ALL STUDENTS INFO */
+app.use('/api/students', require('./api/students/routes/get_students'));
+
+/** *********************************************************** */
+
+/** POST SINGLE STUDENT INFO */
+app.post('/api/student', (req, res) => {
+    
+    const student = new Student(req.body);
+
+    student.save((err, student) => {
+        if(err) {
+            res.status(400).json(err);
+        }
+
+        res.status(200).json(student);
+
+    });
+});
+
+/** *********************************************************** */
+
+// app.use('/api/students', require('./api/students/routes/get_student'));
+
+/** *********************************************************** */
+
+// /** GET SINGLE STUDENT INFO */
+app.get('/api/student/:id', (req, res) => {
+    
+    const _id = req.params.id;
+    
+    Student.findOne({ _id }, (err, student) => {
+        if(err) {
+            res.status(400).json(err);
+        }
+
+        if(!student) {
+            res.status(404).json({ message: 'No student found'});
+        }
+
+        res.status(200).json(student);
+    });
+
+});
+
+/** *********************************************************** */
+
+/** DELETE SINGLE STUDENT */
+app.delete('/api/student/:id' , (req, res) => {
+
+    const _id = req.params.id;
+
+    Student.findOneAndRemove({ _id }, (err, student) => {
+
+        if(err) {
+            res.status(400).json(err);
+        }
+
+        if(!student) {
+            res.status(404).json({ message: 'Student record not found'});
+        }
+
+        res.status(200).json({ message: `Student ${student.id} has been deleted` });
+    });
+
+});
+
+/** *********************************************************** */
+
+/**  EDIT A STUDENT */
+
+app.put('/api/student/:id', (req, res) => {
+
+    const _id = req.params.id;
+    
+    Student.findOneAndUpdate({ _id }, 
+        req.body, { new: true },
+        (err, student) => {
+            if(err) {
+                res.status(400).json(err);
+            }
+
+            res.status(200).json(student);
+    });
+
+});
+
+/** *********************************************************** */
 
 /* CONFIGURE HOSTNAME AND PORT  */
-const hostname = "https://studentia-api.herokuapp.com/";
+const hostname = "localhost";
 const port = 3001;
 
-app.listen(process.env.PORT || 5000, process.env.hostname, () => {
+app.listen(port || 5000, hostname, () => {
 
     mongoose.connect(mongooseUri, dbOptions, (err) => {
         if (err) {
