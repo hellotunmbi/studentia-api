fc9307a2cd5e93cbdc25d23f5e34908828fb99b4
Hello Tunmbi <hellotunmbi@gmail.com>
2017-11-05 14:27:16 +0100

    added sex to model

--- api/students/model/Student.js
+++ api/students/model/Student.js
@@ -6,6 +6,7 @@ const Schema = mongoose.Schema;
 const StudentSchema = new Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
+    sex: { type: String, required: true },
     faculty: { type: String, required: true },
     department: { type: String, required: true },
     level: { type: String, required: true }
