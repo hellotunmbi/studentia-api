9c023d4d86b2f7281af23978169434d6d2cc85f5
Tunmbi Banto <hellotunmbi@gmail.com>
2021-12-25 07:21:41 +0100

    changed server startup line

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./index');
+const app = require("./index");
 
 /* CONFIGURE HOSTNAME AND PORT  */
 // const hostname = "https://studentia-api.herokuapp.com";
@@ -7,9 +7,6 @@ const app = require('./index');
 const hostname = "localhost";
 const port = 3001;
 
-
-app.listen(port || 5000, hostname, () => {
-
-	console.log(`Server started on http://${hostname}:${port}`);
-
+const server = app.listen(process.env.PORT || port, () => {
+  console.log(`Server now up and running on port ${port}`);
 });
