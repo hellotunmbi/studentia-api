9336afa172cd17ceda27b1e2fba998af39b30707
Tunmbi Banto <hellotunmbi@gmail.com>
2021-12-25 07:16:54 +0100

    updated db connection

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-'use strict'
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const uriUtil = require('mongodb-uri');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongodbUri = 'mongodb://student:password@ds143245.mlab.com:43245/studentia-mongo';
-const mongooseUri = uriUtil.formatMongoose(mongodbUri);
-const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true };
-
-require('dotenv').config();
+// Connect to MongoDB...
+let mongoDB = process.env.MONGODB_URL; // || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("> MongoDB Connection Error...");
+});
+db.once("open", () => {
+  console.log("> Successfully Connected the database");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const Student = require('./api/students/model/Student');
-
-
+const Student = require("./api/students/model/Student");
 
 /** *********************************************************** */
 
-app.get('/', (request, response) => {
-	response.status(200).json({ message: 'You successfully connected.' })
+app.get("/", (request, response) => {
+  response.status(200).json({ message: "You successfully connected." });
 });
 
 /** *********************************************************** */
 
-app.get('/api', (request, response) => {
-	response.status(200).json({ message: 'Welcome to API Home.' })
+app.get("/api", (request, response) => {
+  response.status(200).json({ message: "Welcome to API Home." });
 });
 
 /** *********************************************************** */
 
 /** GET ALL STUDENTS INFO */
-app.use('/api/students', require('./api/students/routes/get_students'));
+app.use("/api/students", require("./api/students/routes/get_students"));
 
 /** *********************************************************** */
 
 /** POST SINGLE STUDENT INFO */
-app.post('/api/student', (req, res) => {
+app.post("/api/student", (req, res) => {
+  const student = new Student(req.body);
 
-	const student = new Student(req.body);
+  student.save((err, student) => {
+    if (err) {
+      res.status(400).json(err);
+    }
 
-	student.save((err, student) => {
-		if (err) {
-			res.status(400).json(err);
-		}
-
-		res.status(200).json(student);
-
-	});
+    res.status(200).json(student);
+  });
 });
 
 /** *********************************************************** */
@@ -63,70 +65,57 @@ app.post('/api/student', (req, res) => {
 /** *********************************************************** */
 
 // /** GET SINGLE STUDENT INFO */
-app.get('/api/student/:id', (req, res) => {
+app.get("/api/student/:id", (req, res) => {
+  const _id = req.params.id;
 
-	const _id = req.params.id;
+  Student.findOne({ _id }, (err, student) => {
+    if (err) {
+      res.status(400).json(err);
+    }
 
-	Student.findOne({ _id }, (err, student) => {
-		if (err) {
-			res.status(400).json(err);
-		}
+    if (!student) {
+      res.status(404).json({ message: "No student found" });
+    }
 
-		if (!student) {
-			res.status(404).json({ message: 'No student found' });
-		}
-
-		res.status(200).json(student);
-	});
-
+    res.status(200).json(student);
+  });
 });
 
 /** *********************************************************** */
 
 /** DELETE SINGLE STUDENT */
-app.delete('/api/student/:id', (req, res) => {
+app.delete("/api/student/:id", (req, res) => {
+  const _id = req.params.id;
 
-	const _id = req.params.id;
+  Student.findOneAndRemove({ _id }, (err, student) => {
+    if (err) {
+      res.status(400).json(err);
+    }
 
-	Student.findOneAndRemove({ _id }, (err, student) => {
+    if (!student) {
+      res.status(404).json({ message: "Student record not found" });
+    }
 
-		if (err) {
-			res.status(400).json(err);
-		}
-
-		if (!student) {
-			res.status(404).json({ message: 'Student record not found' });
-		}
-
-		res.status(200).json({ message: `Student ${student.id} has been deleted` });
-	});
-
+    res.status(200).json({ message: `Student ${student.id} has been deleted` });
+  });
 });
 
 /** *********************************************************** */
 
 /**  EDIT A STUDENT */
 
-app.put('/api/student/:id', (req, res) => {
+app.put("/api/student/:id", (req, res) => {
+  const _id = req.params.id;
 
-	const _id = req.params.id;
+  Student.findOneAndUpdate({ _id }, req.body, { new: true }, (err, student) => {
+    if (err) {
+      res.status(400).json(err);
+    }
 
-	Student.findOneAndUpdate({ _id },
-		req.body, { new: true },
-		(err, student) => {
-			if (err) {
-				res.status(400).json(err);
-			}
-
-			res.status(200).json(student);
-		});
-
+    res.status(200).json(student);
+  });
 });
 
 /** *********************************************************** */
-
-
-mongoose.connect(mongooseUri, dbOptions);
-
 
 module.exports = app;
